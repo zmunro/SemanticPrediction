fca5abf3d6b69c0d14061e8ca0988e895f5b4afa
zmunro <zacharymunro2@gmail.com>
2018-11-26 13:04:49 -0500

    need to implement json parsing logic

--- PredictionPlanning.java
+++ PredictionPlanning.java
@@ -1,6 +1,10 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
+  
+import org.json.simple.JSONArray; 
+import org.json.simple.JSONObject; 
+import org.json.simple.parser.*; 
 
 class PredictionsManager {
     // Number of most likely objects/extraItems to keep track of.
@@ -269,9 +273,46 @@ class PredictionPlanning {
     public static void main(String[] args) {
         PredictionsManager pm = new PredictionsManager(3, 3);
         
-        // ObjectMapper mapper = new ObjectMapper();
-		// InputStream is = Test.class.getResourceAsStream("/input.json");
-		// testObj = mapper.readValue(is, Test.class);
-		// System.out.println(testObj.actions[0].name);
+        //  // parsing file "JSONExample.json" 
+        // Object obj = new JSONParser().parse(new FileReader("input.json")); 
+          
+        // // typecasting obj to JSONObject 
+        // JSONObject jo = (JSONObject) obj; 
+          
+        // // getting firstName and lastName 
+        // String firstName = (String) jo.get("firstName"); 
+        // String lastName = (String) jo.get("lastName"); 
+          
+        // System.out.println(firstName); 
+        // System.out.println(lastName); 
+          
+        // // getting age 
+        // long age = (long) jo.get("age"); 
+        // System.out.println(age); 
+          
+        // // getting address 
+        // Map address = ((Map)jo.get("address")); 
+          
+        // // iterating address Map 
+        // Iterator<Map.Entry> itr1 = address.entrySet().iterator(); 
+        // while (itr1.hasNext()) { 
+        //     Map.Entry pair = itr1.next(); 
+        //     System.out.println(pair.getKey() + " : " + pair.getValue()); 
+        // } 
+          
+        // // getting phoneNumbers 
+        // JSONArray ja = (JSONArray) jo.get("phoneNumbers"); 
+          
+        // // iterating phoneNumbers 
+        // Iterator itr2 = ja.iterator(); 
+          
+        // while (itr2.hasNext())  
+        // { 
+        //     itr1 = ((Map) itr2.next()).entrySet().iterator(); 
+        //     while (itr1.hasNext()) { 
+        //         Map.Entry pair = itr1.next(); 
+        //         System.out.println(pair.getKey() + " : " + pair.getValue()); 
+        //     } 
+        // }
 	}
 }
